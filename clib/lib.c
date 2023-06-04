69164765a54020d6fe5d99407b8a6ea470db604e
Arne Philipeit <arne.philipeit@gmail.com>
2023-06-04 10:38:18 -0400

    Move C lib to separate file

---
+++ clib/lib.c
@@ -0,0 +1,56 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* itoa(int x) {
+  char* a = malloc(20);
+  sprintf(a, "%i", x);
+  return a;
+}
+
+char* read(const char* path) {
+  FILE* f = fopen(path, "rb");
+  if (f == NULL) {
+    printf("could not open file: %s\n", path);
+    exit(1);
+  }
+  fseek(f, 0, SEEK_END);
+  int length = ftell(f);
+  char* text = malloc(length);
+  fseek(f, 0, SEEK_SET);
+  fread(text, 1, length, f);
+  fclose(f);
+  return text;
+}
+
+void write(const char* path, const char* text) {
+  FILE *f = fopen(path, "w");
+  if (f == NULL) {
+    printf("could not open file: %s\n", path);
+    exit(1);
+  }
+  fprintf(f, "%s", text);
+  fclose(f);
+}
+
+int _main(char* args);
+
+int main(int argc, char **argv) {
+  int l = 0;
+  for (int i = 1; i < argc; i++) {
+    l += strlen(argv[i]) + 1;
+  }
+  char* args = calloc(l, sizeof(char));
+  char* a = args;
+  for (int i = 1; i < argc; i++) {
+    int c = strlen(argv[i]);
+    strncpy(a, argv[i], c);
+    a += c;
+    a[0] = ' ';
+    a += 1;
+  }
+  args[l-1] = 0;
+  return _main(args);
+}
+
